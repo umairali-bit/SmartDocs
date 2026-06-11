30fa56fc9f20ac0d3043bb883f39fa489cbe42fe
Umair Ali <umairmamoor@gmail.com>
2026-06-11 09:03:37 +0800

    chore: initialize SmartDocs project

---
+++ src/main/java/com/example/smartDocs/SmartDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartDocs/SmartDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
